88786dba15616a7561250c80e72113b873fa0c44
unknown <nerothe42@gmail.com>
2018-01-07 14:26:59 +0100

    Cleaned up the code a lot Added more null checking
    Updated Assetmenu to use Checked Items
    Added "Unity Default for Null" funktionality
    Updated assetmenu.png to show new menu

--- AssetIcons.cs
+++ AssetIcons.cs
@@ -14,40 +14,77 @@ public class ScriptableObjectIcon : Attribute
 [InitializeOnLoad]
 public class AssetIcons : Editor {
 
-    static bool enabled = false;
+    static bool unityDeafaultOnNull;
+    static bool UnityDeafaultOnNull
+    {
+        get
+        {
+            return unityDeafaultOnNull;
+        }
+        set
+        {
+            Menu.SetChecked(MENU_NAME_DefOnNull, value);
+            EditorPrefs.SetBool(MENU_NAME_DefOnNull, value);
+            unityDeafaultOnNull = value;
+        }
+    }
+
+    static bool disableIcons;
+    static bool DisableIcons {
+        get
+        {
+            return disableIcons;
+        }
+        set
+        {
+            disableIcons = value;
+            SetStateDisabled(value);
+        }
+    }
+
     public static Color backgroundColor = new Color(82f / 255f,82f / 255f, 82f / 255f, 1f);
     static Texture2D bg;
 
+    private const string MENU_NAME_DISABLE_ICONS = "Assets/Icons/Disable";
+    private const string MENU_NAME_DefOnNull = "Assets/Icons/Unity default for Null";
+
+
+    [MenuItem(MENU_NAME_DISABLE_ICONS)]
+    private static void ToggleShowIconsAction()
+    {
+        DisableIcons = !DisableIcons;
+    }
+
+    [MenuItem(MENU_NAME_DefOnNull)]
+    private static void ToggleDefOnNullAction()
+    {
+        UnityDeafaultOnNull = !UnityDeafaultOnNull;
+    }
+
     static AssetIcons()
     {
-        EnableIcons();
+        disableIcons = EditorPrefs.GetBool(MENU_NAME_DISABLE_ICONS, false);
+        unityDeafaultOnNull = EditorPrefs.GetBool(MENU_NAME_DefOnNull, false);
+
+        EditorApplication.delayCall += () => {
+            SetStateDisabled(disableIcons);
+            UnityDeafaultOnNull = unityDeafaultOnNull;
+        };
     }
 
-    [MenuItem("Assets/Icons/Enable")]
-    static void EnableIcons()
+    static void SetStateDisabled(bool value)
     {
-        enabled = true;
-        EditorApplication.projectWindowItemOnGUI -= AssetIcons.MyCallback();
-        EditorApplication.projectWindowItemOnGUI += AssetIcons.MyCallback();
-    }
-
-    [MenuItem("Assets/Icons/Enable", true)]
-    private static bool EnableMenuOptionValidation()
-    {
-        return !enabled;
-    }
-
-    [MenuItem("Assets/Icons/Disable")]
-    static void DisableIcons()
-    {
-        enabled = false;
-        EditorApplication.projectWindowItemOnGUI -= AssetIcons.MyCallback();
-    }
-
-    [MenuItem("Assets/Icons/Disable", true)]
-    private static bool DisableMenuOptionValidation()
-    {
-        return enabled;
+        Menu.SetChecked(MENU_NAME_DISABLE_ICONS, value);
+        EditorPrefs.SetBool(MENU_NAME_DISABLE_ICONS, value);
+        if (value)
+        {
+            EditorApplication.projectWindowItemOnGUI -= MyCallback();
+        }
+        else
+        {
+            EditorApplication.projectWindowItemOnGUI -= MyCallback();
+            EditorApplication.projectWindowItemOnGUI += MyCallback();
+        }
     }
 
     static EditorApplication.ProjectWindowItemCallback MyCallback()
@@ -63,35 +100,41 @@ public class AssetIcons : Editor {
         if (bg == null)
         {
             bg = new Texture2D(32, 32);
-            Color[] colors = new Color[32 * 32];
-            for (int i = 0; i < colors.Length; i++)
-            {
-                colors[i] = backgroundColor;
-            }
-            bg.SetPixels(colors);
         }
 
         var t = AssetDatabase.LoadAssetAtPath(guid, typeof(object)) as object;
         if (t == null || t.GetType() == null) return;
         
-        Sprite sprite = null;
         Texture2D texture = null;
         
         var atts = t.GetType().GetFields().Where(fi => ((fi == null) ? 0 : fi.GetCustomAttributes(typeof(ScriptableObjectIcon), false).Count()) > 0);
 
-        if (atts != null && atts.Count() == 1 && atts.First().GetValue(t).GetType() == typeof(Sprite))
+        if (atts != null && atts.Count() == 1 )
         {
-            sprite = (Sprite)atts.First().GetValue(t);
-        }
+            object obj = atts.First().GetValue(t);
+            if (obj == null)
+                return;
 
-        if (atts != null && atts.Count() == 1 && atts.First().GetValue(t).GetType() == typeof(Texture2D))
+            if (obj.GetType() == typeof(Sprite))
+            {
+                Sprite sprite = (Sprite)obj;
+                if (sprite != null)
+                {
+                    texture = sprite.texture;
+                }
+            }
+
+            if (obj.GetType() == typeof(Texture2D))
+                texture = (Texture2D)obj;
+        }
+        else
         {
-            texture = (Texture2D)atts.First().GetValue(t);
-        }
-
-        if (sprite == null && texture == null)
             return;
-        
+        }
+
+        if (texture == null && unityDeafaultOnNull)
+            return;
+
         Rect r2 = new Rect(r);
         r2.height -= 14;
         GUI.DrawTexture(r2, bg, ScaleMode.StretchToFill, false);
@@ -99,9 +142,7 @@ public class AssetIcons : Editor {
         
         r.yMin += 5;
         r.height -= 22;
-
-        if(sprite != null)
-            GUI.DrawTexture(r, sprite.texture, ScaleMode.ScaleToFit);
+        
 
         if(texture != null)
             GUI.DrawTexture(r, texture, ScaleMode.ScaleToFit);
